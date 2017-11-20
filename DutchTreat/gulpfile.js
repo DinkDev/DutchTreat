26aa524b5d7edfafdcb34e62d5e2f32e4f73a621
Dale <dinkhome@hotmail.com>
2017-11-20 06:13:50 -0500

    First pass at using NPM and Gulp as a Bower replacement.

---
+++ DutchTreat/gulpfile.js
@@ -0,0 +1,35 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('clean', function () {
+    // place code for your default task here
+});
+
+var paths = {
+    npm: './node_modules/',
+    webroot: './wwwroot/',
+    bower: './bower_components/',
+    lib: './wwwroot/lib/'
+};
+
+gulp.task('copy', ['clean'], function () {
+    var npm = {
+        'bootstrap': 'bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}',
+        'jquery': 'jquery/dist/jquery*.{js,map}',
+        'jquery-validation': 'jquery-validation/jquery.validate.js',
+        'jquery-validation-unobtrusive': 'jquery-validation-unobtrusive/jquery.validate.unobtrusive.js'
+    }
+
+    for (var destinationDir in npm) {
+        gulp.src(paths.npm + npm[destinationDir])
+            .pipe(gulp.dest(paths.lib + destinationDir));
+    }
+});
