3058eeb008b04f4316147dd033d99d49a4e44ff7
dalethompson <dale.thompson@vastec.com>
2017-11-21 15:36:40 -0500

    Half way through "Using ASP.NET MVC"

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
