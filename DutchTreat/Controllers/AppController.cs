24af2e7b5ea1f23cd447b0c57834518f21415780
Dale <dinkhome@hotmail.com>
2017-11-21 21:06:17 -0500

    Near end of "Using ASP.NET MVC" section. Next part is Model Binding

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -9,5 +9,24 @@ namespace DutchTreat.Controllers
         {
             return View();
         }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
     }
 }
