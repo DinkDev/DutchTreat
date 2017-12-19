6dbc4192e257bfcd8f45c66a8c4795c3e1fd2910
Dale <dinkhome@hotmail.com>
2017-12-19 06:21:30 -0500

    small change in main.js and version downgrade of typescript.

--- DutchTreat/wwwroot/ts/main.js
+++ DutchTreat/wwwroot/ts/main.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var storecustomer_1 = require("./storecustomer");
-var shopper = new storecustomer_1.StoreCustomer('Shawn', 'Wildermuth');
+var Storecustomer = require("./storecustomer");
+var StoreCustomer = Storecustomer.StoreCustomer;
+var shopper = new StoreCustomer('Shawn', 'Wildermuth');
 shopper.showName();
 //# sourceMappingURL=main.js.map
